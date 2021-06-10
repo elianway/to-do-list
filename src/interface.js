7b1026077808a4d579bddcef9206f8897954fb8e
Eli Anway <elianway@Elis-Mac-mini.home>
2021-06-10 08:14:20 -0400

    Created html skeleton and app logic

---
+++ src/interface.js
@@ -0,0 +1,11 @@
+//on page load - updateScreen
+
+//new to-do button event listener
+//dispatches new to-do factory
+//to-do - display name, due date, delete button (updateScreen)
+//updateTaskScreen
+//updateProjectBar
+//selectProject
+//newToDo
+//selectToDo
+//expandToDo
