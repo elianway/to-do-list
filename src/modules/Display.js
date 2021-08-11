793a27063cf7cf9dafcd4d4003c319e32f71edc0
elianway <eli.anway@gmail.com>
2021-08-11 07:45:01 -0400

    Load content

--- src/modules/Display.js
+++ src/modules/Display.js
@@ -14,15 +14,63 @@ export default class Display {
   }
 
   static loadProjects() {
-
+    Storage.getTodoList()
+    .getProjects()
+    .forEach((project) => {
+      if (
+        project.name !== 'Inbox' &&
+        project.name !== 'Today' &&
+        project.name !== 'This week'
+      ) {
+        Display.createProject(project.name)
+      }
+    })
+    Display.initAddProjectButtons()
   }
 
   static loadTasks(projectName) {
+    Storage.getTodoList()
+    .getProject(projectName)
+    .getTasks()
+    .forEach((task) => Display.createTask(task.name, task.dueDate))
 
+    if (projectName !== 'Today' && projectName !== 'This week') {
+      Display.initAddTaskButtons()
+    }
   }
 
   static loadProjectContent(projectName) {
+    const projectPreview = document.getElementById('project-preview')
+    projectPreview.innerHTML = `
+        <h1 id="project-name">${projectName}</h1>
+        <div class="tasks-list" id="tasks-list"></div>`
 
+    if (projectName !== 'Today' && projectName !== 'This week') {
+      projectPreview.innerHTML += `
+        <button class="button-add-task" id="button-add-task">
+          <i class="fas fa-plus"></i>
+          Add Task
+        </button>
+        <div class="add-task-popup" id="add-task-popup">
+          <input
+            class="input-add-task-popup"
+            id="input-add-task-popup"
+            type="text"
+          />
+          <div class="add-task-popup-buttons">
+            <button class="button-add-task-popup" id="button-add-task-popup">
+              Add
+            </button>
+            <button
+              class="button-cancel-task-popup"
+              id="button-cancel-task-popup"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>`
+    }
+    Display.loadTasks(projectName)
   }
 
   //CREATE CONTENT
